c53215c3ef58f52e5bcf753a0da5a10ebc62bc17
qqpp6767 <qqww11eee0@gmail.com>
2026-09-15 21:09:31 +0600

    Добавил начальные переменные (current_day, current_hour, inventory); Придумал предметы для массива inventory; Сделал пробную версию ввода действия.

--- main.c
+++ main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
+int current_day = 1;
+int current_hour = 8;
+int inventory[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+/*
+0 - пусто
+1 - дерево
+2 - камень
+3 - семена
+4 - трава
+5 - грибы
+6 - железо
+7 - песок
+8 - уголь
+9 - золото
+*/
+
 int main(){
-    printf("hey is it working?");
+    int taskNum;
+    do{
+        printf("Введите № действия: ");
+        scanf("%d",&taskNum);
+    } while (taskNum != 0);
     return 0;
 }
