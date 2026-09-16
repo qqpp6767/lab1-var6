672672ece1c81f5b059fd5ecf8035fed7b1212e9
qqpp6767 <qqww11eee0@gmail.com>
2026-09-16 21:11:28 +0600

    Добавил проверку ввода (ввёл букву - переспрашивает до тех пор, пока не будет число) и основу для будущих действий

--- main.c
+++ main.c
@@ -16,11 +16,47 @@ int inventory[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 9 - золото
 */
 
-int main(){
-    int taskNum;
-    do{
-        printf("Введите № действия: ");
-        scanf("%d",&taskNum);
-    } while (taskNum != 0);
-    return 0;
+int main() 
+{
+  int taskNum;
+  do 
+  {
+    printf("Введи № действия (1-6): ");
+    if (scanf("%d", &taskNum) == 0) // проверка ввода
+    {
+      printf("Ты конч? Я просил число\n");
+      while (getchar() != '\n'); // очистка буфера ввода
+      continue;
+    }
+
+    switch (taskNum) 
+    {
+    case 1:
+      printf("1\n");
+      break;
+    case 2:
+      printf("2\n");
+      break;
+    case 3:
+      printf("3\n");
+      break;
+    case 4:
+      printf("4\n");
+      break;
+    case 5:
+      printf("5\n");
+      break;
+    case 6:
+      printf("6\n");
+      break;
+    case 0:
+      printf("Ну пока");
+      break;
+    default:
+      printf("Ты че? только 1-6\n");
+      break;
+    }
+
+  } while (taskNum != 0);
+  return 0;
 }
